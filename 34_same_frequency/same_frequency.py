5f17cc260bd52a860fccae4621681b8f2f5d8f58
Glory <gloryannrios@gmail.com>
2022-01-06 10:47:56 -0500

    first commit

---
+++ 34_same_frequency/same_frequency.py
@@ -0,0 +1,15 @@
+def same_frequency(num1, num2):
+    """Do these nums have same frequencies of digits?
+
+        >>> same_frequency(551122, 221515)
+        True
+
+        >>> same_frequency(321142, 3212215)
+        False
+
+        >>> same_frequency(1212, 2211)
+        True
+    """
+    if sorted(set(str(num1))) == sorted(set(str(num2))):
+        return True
+    return False
